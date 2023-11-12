b0167ba4f7321a02b5cd9592b7ce0ae55f47bcde
zifeng <496435546@qq.com>
2023-11-12 14:14:36 +0800

    拷贝文件

--- packages/zifeng-cli/bin/copy.js
+++ packages/zifeng-cli/bin/copy.js
@@ -21,6 +21,16 @@ function copyDir(form, to, options) {
   copydir.sync(form, to, options);
 }
 
+function copyFile(from, to) {
+  console.log(from, to,1);
+  const buffer = fs.readFileSync(from);
+  const parentPath = path.dirname(to);
+
+  mkdirGuard(parentPath)
+
+  fs.writeFileSync(to, buffer);
+}
+
 function checkMkdirExists(path) {
   return fs.existsSync(path)
 };
@@ -28,3 +38,4 @@ function checkMkdirExists(path) {
 exports.checkMkdirExists = checkMkdirExists;
 exports.mkdirGuard = mkdirGuard;
 exports.copyDir = copyDir;
+exports.copyFile = copyFile;
--- packages/zifeng-cli/bin/index.js
+++ packages/zifeng-cli/bin/index.js
@@ -4,7 +4,7 @@ const yargs = require("yargs")
 const path = require('path');
 
 const { inquirerPrompt } = require('./inquirer')
-const { checkMkdirExists, copyDir } = require("./copy")
+const { checkMkdirExists, copyDir, copyFile } = require("./copy")
 
 yargs.command(
     ['create','c'],
@@ -18,21 +18,41 @@ yargs.command(
         })
     },
     function (argv) {
+        // copy 文件夹
+        // inquirerPrompt(argv).then(answers => {
+        //     const { name, type} = answers;
+        //     const isMkdirExists = checkMkdirExists(
+        //         path.resolve(process.cwd(),`./src/pages/${name}`)
+        //     );
+        //     if (isMkdirExists) {
+        //         console.log(`${name}文件夹已经存在`);
+        //     } else {
+        //         copyDir(
+        //             path.relative(__dirname,`./template/${type}`),
+        //             path.relative(process.cwd(),`./src/pages/${name}`)
+        //         )
+        //     }
+        // })
+
+        // copy 文件
         inquirerPrompt(argv).then(answers => {
             const { name, type} = answers;
             const isMkdirExists = checkMkdirExists(
-                path.resolve(process.cwd(),`./src/pages/${name}`)
+                path.resolve(process.cwd(),`./src/pages/${name}/index.js`)
             );
+            console.log(__dirname,'__dirname',);
             if (isMkdirExists) {
-                console.log(`${name}文件夹已经存在`);
+                console.log(`${name}/index.js文件夹已经存在`);
             } else {
-                copyDir(
-                    path.relative(__dirname,`./template/${type}`),
-                    path.relative(process.cwd(),`./src/pages/${name}`)
+                copyFile(
+                    path.resolve(__dirname,`./template/${type}/index.js`),
+                    path.resolve(process.cwd(),`./src/pages/${name}/index.js`),
+                    {
+                        name,
+                    }
                 )
             }
         })
     }
 ).argv
 
-console.log("welcome to zifeng world!");
